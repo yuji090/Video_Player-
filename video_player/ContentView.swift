979ea6b7eedc3e2198ba5f208a6e57609e37a40b
yuji090 <bishtakhil04@gmail.com>
2024-06-07 10:53:12 +0530

    Initial Commit

---
+++ video_player/ContentView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import AVKit
+import AVFoundation
+
+struct VideoPlayerView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let playerViewController = AVPlayerViewController()
+        if let path = Bundle.main.path(forResource: "SampleVideo_1280x720_1mb", ofType: "mp4") {
+            let player = AVPlayer(url: URL(fileURLWithPath: path))
+            playerViewController.player = player
+        } else {
+            debugPrint("SampleVideo_1280x720_1mb.mp4 not found")
+        }
+        return playerViewController
+    }
+    
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        // No need to update the view controller
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VideoPlayerView()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ video_player/video_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerApp.swift
+//  video_player
+//
+//  Created by Akhil Bisht on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct video_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
